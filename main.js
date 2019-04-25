532adc5fcdd1c488fb170a613dbadc4f7264e189
Leah Hamilton <leahrhamilton@yahoo.com>
2019-04-25 09:12:37 -0400

    final portfolio design

---
+++ main.js
@@ -0,0 +1,44 @@
+'use strict'
+
+//toggle the menu and hamburger 
+/*function hideMenu () {
+    $(window).resize(function(){ 
+        if ($(window).width() > 768) {
+            $('.menu-desktop').show();
+            $('.menu-mobile').hide();
+        } else {
+            $('.desktop', '.menu-desktop').hide();
+            $('.menu-mobile').show();
+        }
+    }
+})*/
+
+function toggleIcon() {
+    $('.icon').on('click', function(){
+        $('.icon').toggleClass('active');
+        $('.menu-mobile').slideToggle(300);
+    })
+}
+
+function closeMobileMenu(){
+    $('.menu-mobile').on('click', 'a', function(){
+        $('.icon').trigger('click');
+    })
+}
+
+
+
+function desktopMenu() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 50 && $(window).width() > 768) {
+            $('header.desktop').fadeIn(500);
+        } else {
+            $('header.desktop').fadeOut(500);
+        }
+    })
+}
+
+//when the page loads call toggleIcon;
+$(toggleIcon);
+$(closeMobileMenu);
+$(desktopMenu);
--- readme.md
+++ readme.md
@@ -12,3 +12,10 @@ My personal portfolio website
 
 * Background Image: Patrick Banld Unsplash
 
+## Thoughts on Improvements
+
+- I would like to make the header have a parallax scroll effect
+- Maybe make the social icons in the contact section grow when you scroll over them
+
+## Icons
+<div>Icons made by <a href="https://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a> from <a href="https://www.flaticon.com/" 			    title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" 			    title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
